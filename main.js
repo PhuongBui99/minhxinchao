bffc93c08f1245c5c05733d111e2b82f0ece6aaf
PhuongBui99 <69580574+PhuongBui99@users.noreply.github.com>
2021-02-26 10:49:44 +0700

    next

---
+++ main.js
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+$(document).ready(function () {
+    $("#single_item").slick({
+      
+    });  
+  });
+=======
+$(document).ready(function () {
+    $("#single_item").slick({
+      
+    });  
+  });
+>>>>>>> ccae73a57ab644846c68d6990fca7ef84ea9363d
+  
